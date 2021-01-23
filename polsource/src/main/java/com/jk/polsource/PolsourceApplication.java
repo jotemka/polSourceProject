c727201d3c358fe4784767a9f460b9a5326bd0c0
J K <jakub.m.krysiak@gmail.com>
2021-01-23 19:58:51 +0100

    Project setup.

---
+++ polsource/src/main/java/com/jk/polsource/PolsourceApplication.java
@@ -0,0 +1,13 @@
+package com.jk.polsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolsourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolsourceApplication.class, args);
+	}
+
+}
---
+++ polsource/src/test/java/com/jk/polsource/PolsourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.polsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolsourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
