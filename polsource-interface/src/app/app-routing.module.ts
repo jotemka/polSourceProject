b9a783de078c113ce4d120901e00163615bac15f
J K <jakub.m.krysiak@gmail.com>
2021-01-28 14:20:06 +0100

    Not found component in webapp

--- polsource-interface/src/app/app-routing.module.ts
+++ polsource-interface/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NoteCreateComponent } from './notes/note-create/note-create.component';
+import { NoteEditComponent } from './notes/note-edit/note-edit.component';
+import { NoteHistoryComponent } from './notes/note-history/note-history.component';
 import { NoteListComponent } from './notes/note-list/note-list.component';
+import { PageNotFoundComponent } from './notes/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/current-notes', pathMatch: 'full'},
   { path: 'current-notes', component: NoteListComponent },
-  { path: 'new-note', component: NoteCreateComponent}
+  { path: 'new-note', component: NoteCreateComponent },
+  { path: 'edit-note/:id', component: NoteEditComponent },
+  { path: 'note-history/:id', component: NoteHistoryComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- polsource-interface/src/app/notes/notes.module.ts
+++ polsource-interface/src/app/notes/notes.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NoteCreateComponent } from './note-create/note-create.component';
-import { NoteListComponent } from './note-list/note-list.component';
+import { NoteListComponent, SortableHeader } from './note-list/note-list.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { RouterModule } from '@angular/router';
 import { NoteReadComponent } from './note-read/note-read.component';
 import { NoteEditComponent } from './note-edit/note-edit.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NoteHistoryComponent } from './note-history/note-history.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 
@@ -15,13 +18,17 @@ import { NoteEditComponent } from './note-edit/note-edit.component';
     NoteListComponent,
     NoteCreateComponent,
     NoteReadComponent,
-    NoteEditComponent
+    NoteEditComponent,
+    NoteHistoryComponent,
+    SortableHeader,
+    PageNotFoundComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    NgbModule
   ]
 })
 export class NotesModule { }
